d16ba161bc8e3fffda49bdb67df028a931fb6260
Bobbychau <160461958+Bobbychau@users.noreply.github.com>
2024-03-06 19:31:42 +0530

    Update main.sol

--- main.sol
+++ main.sol
@@ -2,16 +2,18 @@
 // Compatible with OpenZeppelin Contracts ^5.0.0
 pragma solidity ^0.8.20;
 
-import "@openzeppelin/contracts@5.0.2/token/ERC20/ERC20.sol";
-import "@openzeppelin/contracts@5.0.2/access/Ownable.sol";
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Burnable.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
 
-contract MyToken is ERC20, Ownable {
-    constructor(address initialOwner)
-        ERC20("MyToken", "MTK")
-        Ownable(initialOwner)
+contract PEPETOKEN is ERC20, ERC20Burnable, Ownable {
+    constructor()
+        ERC20("PEPE TOKEN", "KEK")
+        Ownable(msg.sender)
     {}
 
     function mint(address to, uint256 amount) public onlyOwner {
         _mint(to, amount);
     }
+
 }
