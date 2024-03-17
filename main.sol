aa2ca06c5bebd9a8e6e03acd7656b99344b0728e
Bobbychau <160461958+Bobbychau@users.noreply.github.com>
2024-03-17 08:21:42 +0530

    Update main.sol

--- main.sol
+++ main.sol
@@ -1,19 +1,49 @@
 // SPDX-License-Identifier: MIT
-// Compatible with OpenZeppelin Contracts ^5.0.0
-pragma solidity ^0.8.20;
 
-import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
-import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Burnable.sol";
-import "@openzeppelin/contracts/access/Ownable.sol";
+pragma solidity ^0.8.9;
 
-contract PEPETOKEN is ERC20, ERC20Burnable, Ownable {
-    constructor()
-        ERC20("PEPE TOKEN", "KEK")
-        Ownable(msg.sender)
-    {}
+contract MetaToken {
 
-    function mint(address to, uint256 amount) public onlyOwner {
-        _mint(to, amount);
+    // Declare public variables
+    string public tokenName;
+    string public tokenSymbol;
+    uint256 public totalSupply;
+    address public owner;
+
+    // Create a mapping of addresses to balances
+    mapping(address => uint256) public balance;
+
+    // Constructor function that sets the token name, symbol, and owner
+    constructor() {
+        tokenName = "Metacrafters token";
+        tokenSymbol = "META";
+        owner = msg.sender;
     }
 
+    // Modifier that only allows the owner to execute a function
+    modifier onlyOwner() {
+        require(msg.sender == owner, "Only owner can perform this action!");
+        _;
+    }
+
+
+    function mint(address _recipient, uint256 _amount) public onlyOwner {
+        totalSupply += _amount;
+        balance[_recipient] += _amount;
+    }
+
+
+    function burn(uint256 _amount) public {
+        require(balance[msg.sender] >= _amount, "Insufficient balance");
+        totalSupply -= _amount;
+        balance[msg.sender] -= _amount;
+    }
+
+
+    function transfer(address _recipient, uint256 _amount) public {
+        require(msg.sender != _recipient,"You can not transfer token(s) to yourself!");
+        require(balance[msg.sender] >= _amount, "Transfer amount exceeds balance");
+        balance[msg.sender] -= _amount;
+        balance[_recipient] += _amount;
+    }
 }
